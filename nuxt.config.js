bf2d7d96c6c8390dc4e0c7db1401155c4953612a
allen <eepson123@gmail.com>
2021-05-09 23:09:49 +0800

    ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ export default {
         // middleware: 'routerConfig',
         base: baseEnv === 'development' ? '/' : '/spaceP/'
     },
+    target: 'static',
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
         title: 'spaceP',
